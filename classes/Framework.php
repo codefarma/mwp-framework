9cf96c6770ae9b6e4d86731e351bcf0c06762a16
Kevin Carwile <kcarwile@gmail.com>
2018-02-09 08:39:58 -0800

    updated verbage

--- classes/Framework.php
+++ classes/Framework.php
@@ -669,7 +669,7 @@ class Framework extends Plugin
 if ( defined( 'DIR_TESTDATA' ) ) {
 	\$plugin_dir = dirname( dirname( __FILE__ ) );
 	if ( ! file_exists( \$plugin_dir . '/mwp-framework/plugin.php' ) ) {
-		die( 'Error: MWP must be present in ' . \$plugin_dir . '/mwp-framework to run tests on this plugin.' );
+		die( 'Error: MWP Framework must be present in ' . \$plugin_dir . '/mwp-framework to run tests on this plugin.' );
 	}
 	
 	require_once \$plugin_dir . '/mwp-framework/plugin.php';
