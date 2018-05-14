d628cbf02a0a940b10d24f58729d3f60f10a6d61
Kevin Carwile <kcarwile@gmail.com>
2018-05-13 20:43:50 -0700

    enriched default styling of record view template

--- templates/views/management/records/view.php
+++ templates/views/management/records/view.php
@@ -20,4 +20,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
-<pre><?php echo json_encode( $record->dataArray(), JSON_PRETTY_PRINT ); ?></pre>
+<div class="mwp-bootstrap" style="max-width: 1190px; margin: 50px auto;">
+	<table class="table table-striped table-bordered" style="background-color: #fff; box-shadow: 2px 2px 8px #ddd; border-radius: 4px;">
+		<thead>
+			<tr>
+				<th>Property</th>
+				<th>Value</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach( $record->dataArray() as $property => $value ) : ?>
+			<tr>
+				<td><?php echo $property ?></td>
+				<td><pre><?php echo esc_html( $value ) ?></pre></td>
+			</tr>
+			<?php endforeach ?>
+		</tbody>
+	</table>
+</div>
