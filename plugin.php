c712deaef9b4927156f0fd942caa94dc00c65c19
Kevin Carwile <kcarwile@gmail.com>
2018-05-20 20:17:18 -0700

    2.0.4 build

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: MWP Application Framework for WordPress
- * Version: 2.0.3
+ * Version: 2.0.4
  * Description: Provides an object oriented utility framework for building plugins/applications using WordPress.
  * Author: Kevin Carwile
  * Author URI: http://www.millermedia.io/
