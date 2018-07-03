0a5ba2e199f14b3b482c4243abba2cbbed7e1b78
Kevin Carwile <kcarwile@gmail.com>
2018-07-03 12:22:24 -0700

    Removed AdminController pattern class in preference to more generic Controller class

--- classes/Pattern/Controller.php
+++ classes/Pattern/Controller.php
@@ -15,17 +15,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 use MWP\WordPress\AdminPage;
-use MWP\Framework\Pattern\Singleton;
+use MWP\WordPress\PostPage;
 
 /**
- * BaseController Class
+ * Controller Class
  */
-abstract class _AdminController
+abstract class _Controller
 {
 	/**
 	 * @var	Instance Cache
 	 */
-	protected static $_instance = array();
+	protected static $_instances = array();
 	
 	/**
      * Protected constructor to prevent creating a new instance
@@ -45,31 +45,41 @@ abstract class _AdminController
     }
 	
 	/**
-	 * Initialize
+	 * Constructed
 	 *
 	 * @return    void
 	 */
 	protected function constructed()
 	{
-		// initialization routine when the instance is first created
+		// constructor function for when the instance is first created
+	}
+	
+	/**
+	 * Initialize
+	 *
+	 * @return    void
+	 */
+	public function init()
+	{
+		// initialization routine when the controller is used
 	}
 	
 	/**
 	 * @var	array
 	 */
-	protected $config;
+	public $config;
 	
 	/**
 	 * Create Instance
 	 *
 	 * @param	string				$name				The name of the controller
 	 * @throws  \ErrorException
-	 * @return	AdminController
+	 * @return	Controller
 	 */
 	public static function create( $name, $config=[] )
 	{
-		if ( isset( static::$_instance[ get_called_class() ][ $name ] ) ) {
-			throw new \ErrorException( 'Controller instance by that name already exists.' );
+		if ( isset( static::$_instances[ get_called_class() ][ $name ] ) ) {
+			throw new \ErrorException( 'Controller instance by that name already exists. Class: ' . get_called_class() . ', Name: ' . $name );
 		}
 		
 		$instance = new static;
@@ -80,18 +90,22 @@ abstract class _AdminController
 			$instance->registerAdminPage( $instance->config['adminPage'] );
 		}
 		
-		return static::$_instance[ get_called_class() ][ $name ] = $instance;
+		if ( isset( $instance->config['postPage'] ) ) {
+			$instance->registerPostPage( $instance->config['postPage'] );
+		}
+		
+		return static::$_instances[ get_called_class() ][ $name ] = $instance;
 	}
 	
 	/**
 	 * Get a controller instance
 	 *
-	 * @return AdminController|NULL
+	 * @return Controller|NULL
 	 */
 	public static function get( $name )
 	{
-		if ( isset( static::$_instance[ get_called_class() ][ $name ] ) ) {
-			return static::$_instance[ get_called_class() ][ $name ];
+		if ( isset( static::$_instances[ get_called_class() ][ $name ] ) ) {
+			return static::$_instances[ get_called_class() ][ $name ];
 		}
 		
 		return NULL;
@@ -103,15 +117,21 @@ abstract class _AdminController
 	public $adminPage;
 	
 	/**
+	 * @var	MWP\WordPress\PostPage
+	 */
+	public $postPage;
+	
+	/**
 	 * Register the controller as an admin page
 	 *
 	 * @param	array			$options			Admin page options
+	 * @return	AdminPage
 	 */
 	public function registerAdminPage( $options=array() )
 	{
 		$adminPage = new AdminPage;
 		
-		$adminPage->title = isset( $options['title'] ) ? $options['title'] : end( explode( '\\', get_called_class() ) );
+		$adminPage->title = isset( $options['title'] ) ? $options['title'] : array_pop( explode( '\\', get_called_class() ) );
 		$adminPage->menu  = isset( $options['menu'] ) ? $options['menu'] : $adminPage->title;
 		$adminPage->slug  = isset( $options['slug'] ) ? $options['slug'] : sanitize_title( str_replace( '\\', '-', get_called_class() ) );
 		$adminPage->capability = isset( $options['capability'] ) ? $options['capability'] : $adminPage->capability;
@@ -122,10 +142,29 @@ abstract class _AdminController
 		$adminPage->menu_submenu = isset( $options['menu_submenu'] ) ? $options['menu_submenu'] : null;
 		$adminPage->for = isset( $options['for'] ) ? $options['for'] : $adminPage->for;
 		
-		$adminPage->applyToObject( $this );
+		$adminPage->applyToObject( $this, array() );
 		
 		$this->adminPage = $adminPage;
 		return $this->adminPage;
+	}
+	
+	/**
+	 * Register the controller to a specific post page
+	 *
+	 * @param	array			$options			Post page options
+	 * @return	PostPage
+	 */
+	public function registerPostPage( $options=array() )
+	{
+		if ( isset( $options['post_id'] ) ) {
+			$postPage = new PostPage;
+		
+			$postPage->post_id = $options['post_id'];
+			$postPage->applyToObject( $this, array() );
+			
+			$this->postPage = $postPage;
+			return $this->postPage;
+		}		
 	}
 	
 	/**
@@ -136,9 +175,42 @@ abstract class _AdminController
 	 */
 	public function getUrl( $args=array() )
 	{
+		/* Check if we are registered to both front side and admin side pages. Get the url to the contextual page. */
+		if ( isset( $this->adminPage ) and isset( $this->postPage ) ) {
+			return is_admin() ? $this->getAdminUrl( $args ) : $this->getFrontUrl( $args );
+		}
+		
+		/* Return one or the other */
+		return $this->getAdminUrl( $args ) ?: $this->getFrontUrl( $args );
+	}
+	
+	/**
+	 * Get the controller admin side page url
+	 *
+	 * @param	array			$args			Optional query args
+	 * @param	bool|NULL		$network		Flag indicating if url should point to network admin or not. NULL for auto-detect.
+	 * @return	string
+	 */
+	public function getAdminUrl( $args=array() )
+	{
 		if ( isset( $this->adminPage ) ) {
 			$network = $this->adminPage->for == 'all' ? is_network_admin() : $this->adminPage->for == 'network';
 			return add_query_arg( $args, $network ? network_menu_page_url( $this->adminPage->slug, false ) : menu_page_url( $this->adminPage->slug, false ) );
+		}
+		
+		return '';
+	}
+	
+	/**
+	 * Get the controller front side page url
+	 *
+	 * @param	array			$args			Optional query args
+	 * @return	string
+	 */
+	public function getFrontUrl( $args=array() )
+	{
+		if ( isset( $this->postPage ) ) {
+			return add_query_arg( $args, get_permalink( $this->postPage->post_id ) );
 		}
 		
 		return '';
