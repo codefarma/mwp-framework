33e47456aa082e3af320863e58c98962005b8873
Kevin Carwile <kcarwile@gmail.com>
2018-07-03 12:21:21 -0700

    added ability to use the shortcode annotation on a controller class

--- annotations/Shortcode.php
+++ annotations/Shortcode.php
@@ -19,9 +19,11 @@ if ( class_exists( 'MWP\WordPress\Shortcode' ) ) {
 	return;
 }
 
+use MWP\Framework\Framework;
+
 /**
  * @Annotation 
- * @Target( "METHOD" )
+ * @Target( { "METHOD", "CLASS" } )
  */
 class Shortcode extends \MWP\Framework\Annotation
 {
@@ -41,6 +43,30 @@ class Shortcode extends \MWP\Framework\Annotation
 	public function applyToMethod( $instance, $method, $vars=[] )
 	{
 		add_shortcode( $this->name, array( $instance, $method->name ) );		
+	}
+	
+	/**
+	 * Apply to Object
+	 *
+	 * @param	object		$instance		The object which is documented with this annotation
+	 * @param	array		$vars			Persisted variables returned by previous annotations
+	 * @return	array|NULL
+	 */
+	public function applyToObject( $instance, $vars=[] )
+	{
+		/* Run Controller */
+		add_shortcode( $this->name, function( $atts, $content, $name ) use ( $instance ) { 
+			ob_start();
+			$action = Framework::instance()->getRequest()->get( 'do', 'index' );
+			if( is_callable( array( $instance, 'do_' . $action ) ) ) {
+				$output = call_user_func( array( $instance, 'do_' . $action ), $atts, $content, $name );
+			} else {
+				$output = '<strong>Controller Error:</strong> Implement a "do_' . $action . '()" method on this controller to generate the output of this page.</p>';
+			}
+			$buffered_output = ob_get_clean();
+			
+			return $buffered_output . $output;
+		});		
 	}	
 	
 }
