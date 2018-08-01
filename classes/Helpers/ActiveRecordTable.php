bec4ea20107a4e2e81e2422b31fb6bfd990dacf2
Kevin Carwile <kcarwile@gmail.com>
2018-08-01 13:37:51 -0700

    fixed conflict with bulk action form param and active records named 'action'

--- classes/Helpers/ActiveRecordTable.php
+++ classes/Helpers/ActiveRecordTable.php
@@ -580,6 +580,74 @@ class _ActiveRecordTable extends \WP_List_Table
 	}
 	
 	/**
+	 * Display the bulk actions dropdown.
+	 *
+	 * @since 3.1.0
+	 *
+	 * @param string $which The location of the bulk actions: 'top' or 'bottom'.
+	 *                      This is designated as optional for backward compatibility.
+	 */
+	protected function bulk_actions( $which = '' ) {
+		if ( is_null( $this->_actions ) ) {
+			$this->_actions = $this->get_bulk_actions();
+			/**
+			 * Filters the list table Bulk Actions drop-down.
+			 *
+			 * The dynamic portion of the hook name, `$this->screen->id`, refers
+			 * to the ID of the current screen, usually a string.
+			 *
+			 * This filter can currently only be used to remove bulk actions.
+			 *
+			 * @since 3.5.0
+			 *
+			 * @param array $actions An array of the available bulk actions.
+			 */
+			$this->_actions = apply_filters( "bulk_actions-{$this->screen->id}", $this->_actions );
+			$two = '';
+		} else {
+			$two = '2';
+		}
+
+		if ( empty( $this->_actions ) )
+			return;
+
+		echo '<label for="bulk-action-selector-' . esc_attr( $which ) . '" class="screen-reader-text">' . __( 'Select bulk action' ) . '</label>';
+		echo '<select name="_bulk_action' . $two . '" id="bulk-action-selector-' . esc_attr( $which ) . "\">\n";
+		echo '<option value="-1">' . __( 'Bulk Actions' ) . "</option>\n";
+
+		foreach ( $this->_actions as $name => $title ) {
+			$class = 'edit' === $name ? ' class="hide-if-no-js"' : '';
+
+			echo "\t" . '<option value="' . $name . '"' . $class . '>' . $title . "</option>\n";
+		}
+
+		echo "</select>\n";
+
+		submit_button( __( 'Apply' ), 'action', '', false, array( 'id' => "doaction$two" ) );
+		echo "\n";
+	}
+
+	/**
+	 * Get the current action selected from the bulk actions dropdown.
+	 *
+	 * @since 3.1.0
+	 *
+	 * @return string|false The action name or False if no action was selected
+	 */
+	public function current_action() {
+		if ( isset( $_REQUEST['filter_action'] ) && ! empty( $_REQUEST['filter_action'] ) )
+			return false;
+
+		if ( isset( $_REQUEST['_bulk_action'] ) && -1 != $_REQUEST['_bulk_action'] )
+			return $_REQUEST['_bulk_action'];
+
+		if ( isset( $_REQUEST['_bulk_action2'] ) && -1 != $_REQUEST['_bulk_action2'] )
+			return $_REQUEST['_bulk_action2'];
+
+		return false;
+	}
+
+	/**
 	 * REQUIRED! This method dictates the table's columns and titles. This should
 	 * return an array where the key is the column slug (and class) and the value 
 	 * is the column's title text. If you need a checkbox for bulk actions, refer
