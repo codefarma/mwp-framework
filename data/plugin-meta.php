b14212bc6899bb9bd8cc9fefdb45ec7dae0dc367
Kevin Carwile <kcarwile@gmail.com>
2018-09-06 13:53:08 -0700

    2.1.4 release

--- data/plugin-meta.php
+++ data/plugin-meta.php
@@ -8,7 +8,10 @@ return <<<'JSON'
     "description": "Provides an object oriented utility framework for building plugins\/applications using WordPress.",
     "author": "Kevin Carwile",
     "author_url": "http:\/\/www.millermedia.io\/",
-    "version": "2.1.3",
-    "tables": "mwp_queued_tasks"
+    "version": "2.1.4",
+    "tables": [
+        "mwp_queued_tasks"
+    ],
+    "ms_tables": []
 }
 JSON;
