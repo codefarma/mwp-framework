1c970401285101678e81b11d929aacc222df96e0
Kevin Carwile <kevin@miller-media.com>
2020-08-10 18:34:16 -0700

    2.2.7 build

--- data/plugin-meta.php
+++ data/plugin-meta.php
@@ -8,7 +8,7 @@ return <<<'JSON'
     "description": "Provides an object oriented utility framework for building plugins\/applications using WordPress.",
     "author": "Kevin Carwile",
     "author_url": "http:\/\/www.millermedia.io\/",
-    "version": "2.2.6",
+    "version": "2.2.7",
     "tables": [
         "mwp_queued_tasks"
     ],
